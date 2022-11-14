4e5d98e3357d8db52e0713539a161d03cf360773
lakhara <siddharth.lakhara@gmail.com>
2022-11-14 17:27:15 +0530

    add db models

--- ui/models.py
+++ ui/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
-# Create your models here.
+  
+class StudentsList(models.Model):
+  name = models.TextField(max_length=100)
+  age = models.IntegerField()
+  
+  def __str__(self) -> str:
+    return f"Student {self.name}, {self.age} years old"
+  
